8bd7e003569486884a48225f7ecfea4939a92686
JoseMNter <jose.delacruz@nter.es>
2024-12-18 18:43:54 +0100

    Delete TODO-List directory

--- TODO-List/Tareas.py
+++
@@ -1,8 +0,0 @@
-class tarea:
-    def __init__(self,id_tarea,nombre,descripcion,importancia):
-        self.id_tarea=id_tarea
-        self.nombre=nombre
-        self.descripcion=descripcion
-        self.importancia=importancia
-        
-    
--- TODO-List/Usuarios.py
+++
@@ -1,7 +0,0 @@
-class usuario:
-    def __init__(self,id_usuario,nombre,departamento):
-        self.id_usuario=id_usuario
-        self.nombre=nombre
-        self.departamento=departamento
-        
-        
